09511244ed1517237772a11a7eeec0b0abca93f9
aRandomUser1107 <yvela.cline@gmail.com>
2025-05-16 22:57:21 +0700

    update

--- sesi2/src/app/experience/page.tsx
+++ sesi2/src/app/experience/page.tsx
@@ -1,3 +1,33 @@
-export default function experience() {
+"use client";
+import React from "react";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+} from "@/components/ui/resizable-navbar";
+
+const navItems = [
+  { name: "About", link: "/about" },
+  { name: "Resume", link: "/resume" },
+  { name: "Contact", link: "/contact" },
+];
+
+export default function about(){
+    return(
+        <>
+        <Navbar>
+            <NavBody>
+                <NavItems items={navItems} />
+            </NavBody>
+        </Navbar>
+        
+        <div className="flex flex-col items-center p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="mt-12 text-4xl font-bold text-center">
+          This is <span className="text-blue-500">My Experience</span>
+        </h1>
+        <p className="text-center">none</p>
+      </div>
+      </>
+    )
     
 }
--- sesi2/src/app/resume/page.tsx
+++ sesi2/src/app/resume/page.tsx
@@ -1,3 +1,42 @@
-export default function resume() {
+"use client";
+
+import Image from 'next/image'
+import React from "react";
+import {
+  Navbar,
+  NavBody,
+  NavItems,
+} from "@/components/ui/resizable-navbar";
+
+const navItems = [
+  { name: "Experience", link: "/experience" },
+  { name: "About", link: "/about" },
+  { name: "Contact", link: "/contact" },
+];
+
+ 
+export default function Page() {
+  return (
+    <>
+    <Navbar>
+        <NavBody>
+            <NavItems items={navItems} />
+        </NavBody>
+    </Navbar>
+    <div className="flex flex-col items-center p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1 className="mt-12 text-4xl font-bold text-center">
+          This is <span className="text-blue-500">The Resume</span>
+        </h1>
+      </div>
+    <div className='flex justify-center items-center mt-12'>
+        <Image
+        src="/resume template.png"
+        width={300}
+        height={300}
+        alt="image of a resume template"
+        />
+    </div>
+    </>
     
+  )
 }
